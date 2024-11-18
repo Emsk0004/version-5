e82fb0f9e345404e4f307a43cd350ee63d474413
Emsk0004 <emsk0004@stud.kea.dk>
2024-11-18 17:48:32 +0100

    Version 7

--- animation.js
+++ animation.js
@@ -3,26 +3,44 @@ window.addEventListener("load", sidenVises);
 // Opretter vaiabler
 let point;
 let liv;
+let mySpeed;
 
 // Const
 const zombie1 = document.querySelector("#zombie_container");
+const zombie2 = document.querySelector("#zombie_container1");
+const zombie3 = document.querySelector("#zombie_container2");
 const mand1 = document.querySelector("#mand_container");
+const mand2 = document.querySelector("#mand_container1");
 
 function sidenVises() {
   console.log("sidenVises");
   // Skjul andre skærme
+
+  document.querySelector("#game_over").classList.add("skjul");
+  document.querySelector("#level_complete").classList.add("skjul");
+
   // Vis start skærm
+  document.querySelector("#start").classList.remove("skjul");
+
   // Gør startknap klikbar
-  startSpillet(); //HUSK AT SLETTE DETTE TIL SIDST; DET ER KUN FORDI DER INGEN START KNAP ER
+  document.querySelector("#start_knap").addEventListener("click", startSpillet);
 }
 
 function startSpillet() {
   console.log("startSpillet");
 
   // Skjul andre skærme
+
+  document.querySelector("#game_over").classList.add("skjul");
+  document.querySelector("#level_complete").classList.add("skjul");
+  document.querySelector("#start").classList.add("skjul");
+
   // Nulstil point og liv
   point = 0;
   liv = 3;
+
+  //Nulstil mySpeed
+  mySpeed = 1;
 
   // Skriv point og liv ud
   document.querySelector("#score_board").textContent = "Point = " + point;
@@ -36,33 +54,40 @@ function startSpillet() {
   // Timer er færdig
   document.querySelector("#time_sprite").addEventListener("animationend", stopSpillet);
 
-  // Drop-animationer + Random position og random delay
+  // Drop-animationer + Random position og random delay og speed
 
-  zombie1.classList.add("drop", "pos" + nyRand(6), "delay" + nyRand(4));
-  // INGEN DELAY PÅ MANDEN
-  mand1.classList.add("drop", "pos" + nyRand(6));
-
-  // Start drop-animationer på elementer
-  // zombie1.classList.add("drop");
-  // mand1.classList.add("drop");
+  zombie1.classList.add("drop", "pos" + nyRand(6), "delay" + nyRand(4), "speed" + mySpeed);
+  zombie2.classList.add("drop", "pos" + nyRand(6), "delay" + nyRand(4), "speed" + mySpeed);
+  zombie3.classList.add("drop", "pos" + nyRand(6), "delay" + nyRand(4), "speed" + mySpeed);
+  mand1.classList.add("drop", "pos" + nyRand(6), "delay" + nyRand(4), "speed" + mySpeed);
+  // INGEN DELAY PÅ DEN ENE MAND
+  mand2.classList.add("drop", "pos" + nyRand(6), "speed" + mySpeed);
 
   // Gør begge elementer klikbare
   zombie1.addEventListener("mousedown", zombieClickHandler);
+  zombie2.addEventListener("mousedown", zombieClickHandler);
+  zombie3.addEventListener("mousedown", zombieClickHandler);
   mand1.addEventListener("mousedown", mandClickHandler);
+  mand2.addEventListener("mousedown", mandClickHandler);
 
   // Lyt efter drop-animation på zombie kørt en gang
   zombie1.addEventListener("animationiteration", ZombieDropReset);
+  zombie2.addEventListener("animationiteration", ZombieDropReset);
+  zombie3.addEventListener("animationiteration", ZombieDropReset);
   mand1.addEventListener("animationiteration", MandDropReset);
+  mand2.addEventListener("animationiteration", MandDropReset);
 }
 
 function zombieClickHandler() {
   console.log("zombieClickHandler");
 
+  console.log(this);
+
   // Fjern dobbelt klik
-  zombie1.removeEventListener("mousedown", zombieClickHandler);
+  this.removeEventListener("mousedown", zombieClickHandler);
 
   // frys drop animation
-  zombie1.classList.add("frys");
+  this.classList.add("frys");
 
   // Få 1 point
   point++;
@@ -71,29 +96,42 @@ function zombieClickHandler() {
   document.querySelector("#score_board").textContent = "Point = " + point;
 
   // Afspil god lyd
+
+  // Undersøg om points er 2 eller derover, hvis den er det sæt speed til 2
+
+  // på 5 points skal speed blive 3
+
+  if (point >= 5) {
+    // hvis det er sandt
+    mySpeed = 3;
+  } else if (point >= 2) {
+    // hvis det er sandt
+    mySpeed = 2;
+  }
+
   // Start forsvind-animation
 
-  zombie1.firstElementChild.classList.add("forsvind");
+  this.firstElementChild.classList.add("forsvind");
 
   //Lyt efter forsvind-animation færdig
-  zombie1.addEventListener("animationend", ZombieClickReset);
+  this.addEventListener("animationend", ZombieClickReset);
 }
 
 function ZombieClickReset() {
   console.log("ZombieClickReset");
 
   // Nulstil klasserne på zombien for at fjerne forsvind-animation og tidligere position
-  zombie1.classList = ""; // Fjern alle klasser
-  zombie1.firstElementChild.classList = ""; // Fjern forsvind-effekt på sprite
+  this.classList = ""; // Fjern alle klasser
+  this.firstElementChild.classList = ""; // Fjern forsvind-effekt på sprite
 
   // Tving reflow for at nulstille animationen
-  zombie1.offsetWidth;
+  this.offsetWidth;
 
-  // Tilføj klasserne igen med nye positioner og delay, så zombien starter sin animation forfra
-  zombie1.classList.add("drop", "pos" + nyRand(6), "delay" + nyRand(4));
+  // Tilføj klasserne igen med nye positioner og delay + speed, så zombien starter sin animation forfra
+  this.classList.add("drop", "pos" + nyRand(6), "delay" + nyRand(4), "speed" + mySpeed);
 
   // Gør zombien klikbar igen
-  zombie1.addEventListener("mousedown", zombieClickHandler);
+  this.addEventListener("mousedown", zombieClickHandler);
 }
 
 function ZombieDropReset() {
@@ -107,12 +145,12 @@ function ZombieDropReset() {
 
   // Genstart drop-animation
 
-  zombie1.firstElementChild.classList = "";
-  zombie1.addEventListener("mousedown", zombieClickHandler);
+  this.firstElementChild.classList = "";
+  this.addEventListener("mousedown", zombieClickHandler);
 
-  zombie1.classList = ""; // Nulstil alle klasser
-  zombie1.offsetWidth; // Tving reflow for at nulstille animationen
-  zombie1.classList.add("drop", "pos" + nyRand(6), "delay" + nyRand(4)); // Tilføj klasser igen
+  this.classList = ""; // Nulstil alle klasser
+  this.offsetWidth; // Tving reflow for at nulstille animationen
+  this.classList.add("drop", "pos" + nyRand(6), "delay" + nyRand(4), "speed" + mySpeed); // Tilføj klasser igen
 
   // Mist et liv
   liv--;
@@ -128,10 +166,10 @@ function mandClickHandler() {
   console.log("mandClickHandler");
 
   // Fjern dobbelt klik
-  mand1.removeEventListener("mousedown", mandClickHandler);
+  this.removeEventListener("mousedown", mandClickHandler);
 
   // Frys drop animation
-  mand1.classList.add("frys");
+  this.classList.add("frys");
 
   // Mist et point
   point--;
@@ -141,10 +179,10 @@ function mandClickHandler() {
 
   // Afspil dårlig lyd
   // Start forsvind-animation
-  mand1.firstElementChild.classList.add("zoom_in");
+  this.firstElementChild.classList.add("zoom_in");
 
   //Lyt efter forsvind-animation færdig
-  mand1.addEventListener("animationend", MandClickReset);
+  this.addEventListener("animationend", MandClickReset);
 }
 
 function MandClickReset() {
@@ -156,17 +194,17 @@ function MandClickReset() {
   // Genstart drop-animation
 
   // Fjern alle klasser for at nulstille animation og position
-  mand1.classList = ""; // Nulstil klasser på manden
-  mand1.firstElementChild.classList = ""; // Nulstil eventuelle animationer på sprite
+  this.classList = ""; // Nulstil klasser på manden
+  this.firstElementChild.classList = ""; // Nulstil eventuelle animationer på sprite
 
   // Tving reflow for at sikre, at klassen nulstilles
-  mand1.offsetWidth;
+  this.offsetWidth;
 
-  // Tilføj klasserne igen med ny tilfældig position
-  mand1.classList.add("drop", "pos" + nyRand(6));
+  // Tilføj klasserne igen med ny tilfældig position + delay + speed.
+  this.classList.add("drop", "pos" + nyRand(6), "delay" + nyRand(4), "speed" + mySpeed);
 
   // Gør manden klikbar igen
-  mand1.addEventListener("mousedown", mandClickHandler);
+  this.addEventListener("mousedown", mandClickHandler);
 }
 
 function MandDropReset() {
@@ -177,12 +215,12 @@ function MandDropReset() {
 
   // Genstart drop-animation
 
-  mand1.firstElementChild.classList = "";
-  mand1.addEventListener("mousedown", mandClickHandler);
+  this.firstElementChild.classList = "";
+  this.addEventListener("mousedown", mandClickHandler);
 
-  mand1.classList = ""; // Nulstil alle klasser
-  mand1.offsetWidth; // Tving reflow for at nulstille animationen
-  mand1.classList.add("drop", "pos" + nyRand(6)); // Tilføj klasser igen
+  this.classList = ""; // Nulstil alle klasser
+  this.offsetWidth; // Tving reflow for at nulstille animationen
+  this.classList.add("drop", "pos" + nyRand(6), "delay" + nyRand(4), "speed" + mySpeed); // Tilføj klasser igen
 }
 
 function stopSpillet() {
@@ -199,6 +237,18 @@ function stopSpillet() {
   zombie1.removeEventListener("mousedown", zombieClickHandler);
   zombie1.removeEventListener("animationend", ZombieClickReset);
 
+  zombie2.classList = "";
+  zombie2.firstElementChild.classList = "";
+  zombie2.removeEventListener("animationiteration", ZombieDropReset);
+  zombie2.removeEventListener("mousedown", zombieClickHandler);
+  zombie2.removeEventListener("animationend", ZombieClickReset);
+
+  zombie3.classList = "";
+  zombie3.firstElementChild.classList = "";
+  zombie3.removeEventListener("animationiteration", ZombieDropReset);
+  zombie3.removeEventListener("mousedown", zombieClickHandler);
+  zombie3.removeEventListener("animationend", ZombieClickReset);
+
   // Fjern alle animationer og fjern alle eventListerner-ere på mand
   mand1.classList = "";
   mand1.firstElementChild.classList = "";
@@ -206,11 +256,17 @@ function stopSpillet() {
   mand1.removeEventListener("mousedown", mandClickHandler);
   mand1.removeEventListener("animationend", MandClickReset);
 
+  mand2.classList = "";
+  mand2.firstElementChild.classList = "";
+  mand2.removeEventListener("animationiteration", MandDropReset);
+  mand2.removeEventListener("mousedown", mandClickHandler);
+  mand2.removeEventListener("animationend", MandClickReset);
+
   // Tjek om jeg har mere end 10 point
 
   if (liv <= 0) {
     gameOver();
-  } else if (point >= 4) {
+  } else if (point >= 10) {
     levelComplete();
   }
 
@@ -225,17 +281,19 @@ function stopSpillet() {
 function gameOver() {
   console.log("gameOver");
 
-  // Vis gameover skærm
-
-  // Gør genstart klikbar
+  //Vis gameover skærm
+  document.querySelector("#game_over").classList.remove("skjul");
+  //Klik på genstart1
+  document.querySelector("#genstart1").addEventListener("click", startSpillet);
 }
 
 function levelComplete() {
   console.log("levelComplete");
 
   // Vis levelcomplete skærm
-
-  // Gør genstart klikbar
+  document.querySelector("#level_complete").classList.remove("skjul");
+  // Gør genstart2 klikbar
+  document.querySelector("#genstart2").addEventListener("click", startSpillet);
 }
 
 function nyRand(max) {
